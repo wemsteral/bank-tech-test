f651bfd20d5428c48c2b7217fc11b51d0dbe3e89
Alasdair <alasdair123@hotmail.co.uk>
2019-06-03 11:07:11 +0100

    add deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -7,4 +7,11 @@ class Account
   def initialize
     @balance = 0
   end
+
+  def deposit(deposit_amount)
+    @balance += deposit_amount
+  end
+
+  def withdraw(withdrawal_amount)
+  end 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,12 @@ describe Account do
   it "initialises with empty account" do
     expect(subject.balance).to eq 0
   end
+
+  it "responds to 'deposit'" do
+    expect(subject).to respond_to :deposit
+  end
+
+  it "allows user to deposit a chosen amount of money" do
+    expect { subject.deposit(100) }.to change { subject.balance }.from(0).to(100)
+  end
 end
